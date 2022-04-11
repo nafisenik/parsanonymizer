5284065bbc6487fd23ee2a7d7e5f6c55cfcb69aa
MohammadHadi Hajihosseini <m.hadi.hajihosseini@gmail.com>
2022-04-11 18:33:56 +0430

    add regex for url

--- example.py
+++ example.py
@@ -2,9 +2,11 @@ from parsanonymizer import Model
 
 m = Model()
 
-sent = "نفیسه و علی و @sdfafs مح a@s.com مد و sss زهرا - محمدی ss.sawe_c@sdgm.c به شرکت گوگل و یاهو رفتند و   1237651212 حسن جعفری و اکبری و محمدی sss@sss.com و امیری به آقای محمد حسینی سلام کردند."
+sent = "نفیسه و علی و @sdfafs مح a@s.com مد و sss زهرا - محمدی ss.sawe_c@sdgm.c به شرکت گوگل و یاهو https://google.com رفتند و   1237651212 حسن جعفری و اکبری و hello.com محمدی sss@sss.com و امیری به آقای محمد حسینی سلام کردند."
 # sent = 'کد ملی0924134127 است '
 # sent = 'علیپور علی‌پور رفت '
+# sent = "https://google.com"
+# sent = "this is my حات جطرو https://google.com/hi سلام hello"
 spans = m.extract_span(sent)
 
 with open('out.txt', 'w', encoding='utf-8-sig') as f:
